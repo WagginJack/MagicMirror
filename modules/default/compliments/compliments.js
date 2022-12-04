0b036bb941b612bee0d7b482b4762481c1713489
Ronald Heminway <Ronald.Heminway@ku.edu>
2022-12-04 11:12:01 -0600

    changed location of js facefilter

--- modules/default/compliments/compliments.js
+++ modules/default/compliments/compliments.js
@@ -8,7 +8,7 @@ Module.register("compliments", {
 	// Module config defaults.
 	defaults: {
 		compliments: {
-			anytime: ["Hey there sexy!"],
+			anytime: ["You are looking good!"],
 			morning: ["Good morning, handsome!", "Enjoy your day!", "How was your sleep?"],
 			afternoon: ["Hello, beauty!", "You look sexy!", "Looking good today!"],
 			evening: ["Wow, you look hot!", "You look nice!", "Hi, sexy!"],
