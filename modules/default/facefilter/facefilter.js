aa46860080f3130ad8c6e1b7026810b249055f1b
realVinayak <realvinayak123@gmail.com>
2022-12-11 12:33:23 -0600

    Add facefilter module

---
+++ modules/default/facefilter/facefilter.js
@@ -0,0 +1,38 @@
+Module.register("facefilter", {
+    start: function() {
+        Log.info("Starting module: " + this.name);
+        const maxWidth = Math.min(windowWidth, windowHeight);
+        pixelDensity(1);
+        outputWidth = maxWidth;
+        outputHeight = maxWidth * 0.75; // 4:3
+
+        cnv = createCanvas(outputWidth, outputHeight);
+        cnv.parent('miCamara');
+
+        // webcam capture
+        videoInput = createCapture(VIDEO); //document.getElementById('miCamara');
+        videoInput.parent('miCamara');
+        videoInput.size(outputWidth, outputHeight);
+        videocamara = videoInput;
+        videoInput.hide();
+
+        // tracker
+        faceTracker = new clm.tracker();
+        faceTracker.init();
+        faceTracker.start(videoInput.elt);
+    },
+    maxWidthValue : function(){
+        
+    }
+    videoInputObject : function() {
+        videoInput = createCapture(VIDEO); //document.getElementById('miCamara');
+        videoInput.parent('miCamara');
+        videoInput.size(outputWidth, outputHeight);
+        videocamara = videoInput;
+        videoInput.hide();
+        return videoInput
+    }
+    getDom: function() {
+
+    }
+ });
